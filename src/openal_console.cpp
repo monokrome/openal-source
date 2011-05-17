7d2b0ce67a4593011a233263a53a0aa479eeab02
Brandon R. Stoner <monokrome@monokro.me>
2011-05-17 08:14:00 -0600

    Updated OpenAL console.

--- src/openal_console.cpp
+++ src/openal_console.cpp
@@ -8,8 +8,8 @@
 #include "openal_mp3sample.h"
 //#include "c_basehlplayer.h"
 
-#define OGG_DEMO_FILENAME "demo/demo.ogg"
-#define POS_DEMO_FILENAME "demo/positional.ogg"
+// Defines differt strings that are used for OpenAL's console commands. %s = file extension
+#define OPENAL_DEMO_FILENAME "demo/demo.%s"
 
 IOpenALSample* demoSample = NULL;
 
@@ -20,18 +20,21 @@ void OpenALStopDemo()
 {
     if (demoSample != NULL)
     {
-        demoSample->Stop();
+		if (demoSample->IsPlaying())
+			demoSample->Stop();
     }
 }
 
 /***
- * Plays a nice little looping demo local to the player position.
- **/
-void OpenALPlayDemo(void)
+ * Receives a file extension, and attempts to load and play demo/demo.<fileExtension>.
+ */
+void OpenALStartDemo(char *fileExtension)
 {
+	OpenALStopDemo();
+
     if (demoSample == NULL)
     {
-        demoSample = g_OpenALLoader.Load("ogg");
+        demoSample = g_OpenALLoader.Load(fileExtension);
 
         if (demoSample == NULL)
         {
@@ -39,167 +42,33 @@ void OpenALPlayDemo(void)
         }
     }
 
-    if (demoSample->IsPlaying())
-    {
-        demoSample->Stop();
-    }
-
-    demoSample->Open(OGG_DEMO_FILENAME);
+	demoSample->Open(VarArgs(OPENAL_DEMO_FILENAME, fileExtension));
     demoSample->Play();
 }
 
-
-/***
- * Plays a simple demo in 3D space. Puts the audio source where the player is standing.
- **/
-void OpenALPlayPositionalDemo(void)
+void OpenALOggDemo(void)
 {
-    Warning("Positional audio currently out of order!!!\n");
-    return;
-
-    // Todo: Re-implement this through the sample pool
-
-    /*
-	CBasePlayer* localPlayer = CBasePlayer::GetLocalPlayer();
-
-	OpenALStopDemo();
-
-	demoSample = g_OpenALLoader.Load("ogg");
-	demoSample->SetLooping(true);
-
-	if (localPlayer)
-	{
-		demoSample->SetPositional(true);
-
-		// Does localPlayer need locked now? :/
-		demoSample->SetPosition(localPlayer->GetAbsOrigin());
-		demoSample->SetVelocity(localPlayer->GetAbsVelocity());
-	}
-	else
-	{
-		Msg("OpenAL: Demo can't find local player. Local playback enabled instead of positional.\n");
-		demoSample->SetPositional(false);
-	}
-
-	demoSample->Open(POS_DEMO_FILENAME);
-
-	if (demoSample->IsReady())
-		demoSample->Play();
-    */
+	OpenALStartDemo("ogg");
 }
 
-ConCommand openal_play_demo("openal_play_demo", OpenALPlayDemo, "Play the demo of OpenAL's ogg playback.");
-ConCommand openal_play_positional_demo("openal_play_positional_demo", OpenALPlayPositionalDemo, "Play a demo of using OpenAL for positional audio.");
+void OpenALFlacDemo(void)
+{
+	OpenALStartDemo("flac");
+}
+
+void OpenALMP3Demo(void)
+{
+	OpenALStartDemo("mp3");
+}
+
+void OpenALWavDemo(void)
+{
+	OpenALStartDemo("wav");
+}
+
+ConCommand openal_ogg_demo("openal_ogg_demo", OpenALOggDemo, "Play a demo using OpenAL/ogg support.");
+ConCommand openal_flac_demo("openal_flac_demo", OpenALFlacDemo, "Play a demo using OpenAL/flac support.");
+ConCommand openal_mp3_demo("openal_mp3_demo", OpenALMP3Demo, "Play a demo using OpenAL/mp3 support.");
+ConCommand openal_wav_demo("openal_wav_demo", OpenALWavDemo, "Play a demo using OpenAL/wav support.");
+
 ConCommand openal_stop_demo("openal_stop_demo", OpenALStopDemo, "Stop the current OpenAL playback demo.");
-
-#define WAV_SAMPLE "demo/wave_playback.wav"
-
-IOpenALSample *wavSample = NULL;
-
-void OpenALWavStop()
-{
-    if (wavSample != NULL)
-    {
-        wavSample->Stop();
-    }
-}
-
-/***
- * Plays an even nicer little demo local to the player position.
- **/
-void OpenALWavStart()
-{
-    if (wavSample == NULL)
-    {
-        wavSample = g_OpenALLoader.Load("wav");
-
-        if (wavSample == NULL)
-        {
-            return;
-        }
-    }
-
-    if (wavSample->IsPlaying())
-    {
-        wavSample->Stop();
-    }
-
-    wavSample->Open(WAV_SAMPLE);
-    wavSample->Play();
-}
-
-ConCommand openal_wav_demo_play("openal_wav_demo_play", OpenALWavStart, "Play the demo of OpenAL's wav playback.");
-ConCommand openal_wav_demo_stop("openal_wav_demo_stop", OpenALWavStop, "Stop the demo of OpenAL's wav playback.");
-
-#define FLAC_SAMPLE "demo/demo.flac"
-
-IOpenALSample *flacSample = NULL;
-
-void OpenALFLACStop()
-{
-    if (flacSample != NULL)
-    {
-        flacSample->Stop();
-    }
-}
-
-void OpenALFLACStart()
-{
-    if (flacSample == NULL)
-    {
-        flacSample = g_OpenALLoader.Load("flac");
-
-        if (flacSample == NULL)
-        {
-            return;
-        }
-    }
-
-    if (flacSample->IsPlaying())
-    {
-        flacSample->Stop();
-    }
-
-    flacSample->Open(FLAC_SAMPLE);
-    flacSample->Play();
-}
-
-ConCommand openal_flac_demo_play("openal_flac_demo_play", OpenALFLACStart, "Play the demo of OpenAL's FLAC playback.");
-ConCommand openal_flac_demo_stop("openal_flac_demo_stop", OpenALFLACStop, "Stop the demo of OpenAL's FLAC playback.");
-
-
-#define MP3_SAMPLE "demo/demo.mp3"
-
-IOpenALSample *mp3Sample = NULL;
-
-void OpenALMP3Stop()
-{
-    if (mp3Sample != NULL)
-    {
-        mp3Sample->Stop();
-    }
-}
-
-void OpenALMP3Start()
-{
-    if (mp3Sample == NULL)
-    {
-        mp3Sample = g_OpenALLoader.Load("mp3");
-
-        if (mp3Sample == NULL)
-        {
-            return;
-        }
-    }
-
-    if (mp3Sample->IsPlaying())
-    {
-        mp3Sample->Stop();
-    }
-
-    mp3Sample->Open(MP3_SAMPLE);
-    mp3Sample->Play();
-}
-
-ConCommand openal_mp3_demo_play("openal_mp3_demo_play", OpenALMP3Start, "Play the demo of OpenAL's FLAC playback.");
-ConCommand openal_mp3_demo_stop("openal_mp3_demo_stop", OpenALMP3Stop, "Stop the demo of OpenAL's FLAC playback.");
